8ba60635c49b0a814fda171603ce251c3ed0ff0a
Sid <siddhata.s.patil@gmail.com>
2016-07-06 10:22:52 -0700

    Create qs2.cpp

---
+++ qs2.cpp
@@ -0,0 +1,19 @@
+/*
+ ==============================================================================================================================
+ Name        : qs2.cpp
+ Author      : Siddhata Patil
+ Copyright   : Copyright ©  Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright ©  Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Question    : Think about how many slots of memory are needed for this program:
+               int main ()
+               {
+                  int i;
+                  int votes[ 10 ];
+                }
+ ==============================================================================================================================
+ */
+
+Consider int is 4 bytes
+So, int i = 4 bytes
+  + int votes[10] = 4bytes * 10 = 40 bytes
+So total 44 bytes in memory.
